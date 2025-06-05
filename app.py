be3d6b68e713d5ec2f69ec8aa13eb31432fbb3db
Beto-SantiagoB <asb9904@gmail.com>
2025-06-05 00:53:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ from difflib import get_close_matches
 # import openai  # Comentado temporalmente por falta de crédito
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
+import json
 
 # Configuración de Firebase desde st.secrets
 if not firebase_admin._apps:
-    cred = credentials.Certificate(st.secrets["firebase"])
+    cred = credentials.Certificate(json.loads(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred, {
         'storageBucket': 'inventario-lab-c0974.appspot.com'
     })
